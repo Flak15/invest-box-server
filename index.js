0527f109a1c8c65fef33f8a7537e51153d127f33
flak15 <flak160@gmail.com>
2019-07-17 09:20:48 +0300

    connect config

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const config = require('config');
+
 var express = require('express');
 var app = express();
 const basicAuth = require('express-basic-auth')
+const usersDB = config.get('auth');
 
-app.use(basicAuth({
-    users: { 'admin': 'supersecret' }
-}));
+app.use(basicAuth(usersDB));
 
 app.get('/', function (req, res) {
-  res.send('Hello World! ' + req.auth.user);
+  res.send('Hello World! ');
 });
 
 app.listen(4000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 4000!');
 });
